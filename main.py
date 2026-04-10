f08a84395cc8ef95b8bfb24246aecad7801b66e1
Alfiya-pixel <alfiyaasma56@gmail.com>
2026-04-10 19:00:03 +0530

    setup for railway deployment

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, jsonify  # Import jsonify
 import numpy as np
 import pandas as pd
 import pickle
+import os
 
 
 # flask app
@@ -115,6 +116,6 @@ def blog():
     return render_template("blog.html")
 
 
-if __name__ == '__main__':
-
-    app.run(debug=True)
+if __name__ == "__main__":
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
